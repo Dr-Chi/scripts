beb24befbb8495cb69633af94aa947e68e546bbd
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-04-24 12:01:18 -0400

    Update kali_persistence.sh
    added a warning

--- linux/kali_persistence.sh
+++ linux/kali_persistence.sh
@@ -15,6 +15,15 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit
 fi
 
+echo -e "\033[0;33m******************************************************************"
+echo -e "\033[0;30mWARNING: This will mess with partitions!  You could LOSE data!"
+echo -e "\033[0;33mPlease read everything carefully and only proceed if you know"
+echo -e "\033[0;33mwhat you're doing"
+echo -e "\033[0;33m******************************************************************"
+
+
+read -n 1 -s -r -p "Press any key to continue"
+
 
 fdisk -l
 
@@ -29,6 +38,7 @@ n
 
 
 
+Y
 w
 EOI
 
