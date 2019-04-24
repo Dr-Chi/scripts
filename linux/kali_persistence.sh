fb6d66732019de38cc6b10f7972f1e3d49fb11ff
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-04-24 11:50:27 -0400

    Update kali_persistence.sh
    changed echo to be orange.

--- linux/kali_persistence.sh
+++ linux/kali_persistence.sh
@@ -6,12 +6,12 @@
 
 # Detect users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
-	echo "This script needs to be run with bash, not sh"
+	echo -e "\033[0;33mThis script needs to be run with bash, not sh"
 	exit
 fi
 
 if [[ "$EUID" -ne 0 ]]; then
-	echo "Sorry, you need to run this as root"
+	echo -e "\033[0;33mSorry, you need to run this as root"
 	exit
 fi
 
@@ -19,7 +19,7 @@ fi
 fdisk -l
 
 # is the usb disk /dev/sdb ?
-echo "What is the path to your USB device? Remember: sda is usually the internal HD!"
+echo -e "\033[0;33mWhat is the path to your USB device? Remember: sda is usually the internal HD!"
 read -p "USB path: " -e -i /dev/sdb USB
 set -x #echo on
 # The fdisk options are; n for new part, enter 4 times, w to write the changes
@@ -34,26 +34,26 @@ EOI
 
 fdisk -l    #is it created?  Is is /dev/sdb3?
 set +x #echo off
-echo "Is the new partition /dev/sdb3?"
+echo -e "\033[0;33mIs the new partition /dev/sdb3?"
 read -p "new partition path: " -e -i /dev/sdb3 PAR
-echo "Creating an ext4 filesystem on the new partition"
+echo -e "\033[0;33mCreating an ext4 filesystem on the new partition"
 set -x #echo on
 mkfs.ext4 -L persistence $PAR
 # wait
 sleep 5
 set +x #echo off
-echo "Making sure the disk is labeled"
+echo -e "\033[0;33mMaking sure the disk is labeled"
 set -x #echo on
 e2label $PAR persistence
 set +x #echo off
-echo "Creating a persistence.conf file"
+echo -e "\033[0;33mCreating a persistence.conf file"
 set -x #echo on
 mkdir -p /mnt/storage
 mount $PAR /mnt/storage
-echo "/ union" > /mnt/storage/persistence.conf
+echo -e "\033[0;33m/ union" > /mnt/storage/persistence.conf
 umount $PAR
 set +x #echo off
 echo \n
 echo \n
 echo \n
-echo "DONE!"
+echo -e "\033[0;33mDONE!"
