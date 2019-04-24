161b54786019dcd11e612b2587f0c5853340d195
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-04-24 11:42:46 -0400

    Update kali_persistence.sh
    fixed some echo issues

--- linux/kali_persistence.sh
+++ linux/kali_persistence.sh
@@ -3,7 +3,6 @@
 # https://github.com/Dr-Chi/scripts/master/linux/kali_persistence.sh
 #
 # Copyright (c) 2019 DrChi. Released under the MIT License.
-set -x #echo on
 
 # Detect users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
@@ -22,7 +21,7 @@ fdisk -l
 # is the usb disk /dev/sdb ?
 echo "What is the path to your USB device? Remember: sda is usually the internal HD!"
 read -p "USB path: " -e -i /dev/sdb USB
-
+set -x #echo on
 # The fdisk options are; n for new part, enter 4 times, w to write the changes
 fdisk $USB <<EOI
 n
@@ -34,19 +33,26 @@ w
 EOI
 
 fdisk -l    #is it created?  Is is /dev/sdb3?
+set +x #echo off
 echo "Is the new partition /dev/sdb3?"
 read -p "new partition path: " -e -i /dev/sdb3 PAR
 echo "Creating an ext4 filesystem on the new partition"
+set -x #echo on
 mkfs.ext4 -L persistence $PAR
 # wait
 sleep 5
+set +x #echo off
 echo "Making sure the disk is labeled"
+set -x #echo on
 e2label $PAR persistence
+set +x #echo off
 echo "Creating a persistence.conf file"
+set -x #echo on
 mkdir -p /mnt/storage
 mount $PAR /mnt/storage
 echo "/ union" > /mnt/storage/persistence.conf
 umount $PAR
+set +x #echo off
 echo \n
 echo \n
 echo \n
