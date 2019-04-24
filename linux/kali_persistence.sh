f028a459bc2971956ec2a5068d06fd33eb89ff76
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-04-24 11:02:26 -0400

    Create kali_persistence.sh
    initial file

---
+++ linux/kali_persistence.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# https://github.com/
+#
+# Copyright (c) 2019 DrChi. Released under the MIT License.
+
+
+# Detect users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo "This script needs to be run with bash, not sh"
+	exit
+fi
+
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit
+fi
+
+
+fdisk -l
+
+# is the usb disk /dev/sdb ?
+echo "What is the path to your USB device? Remember: sda is usually the internal HD!"
+read -p "USB path: " -e -i /dev/sdb USB
+fdisk $USB
+# n :for new partition
+n
+# enter 4 times
+echo -en "\n\n\n\n"
+# w :to write the changes
+w
+
+fdisk -l    #is it created?  Is is /dev/sdb3?
+echo "Is the new partition /dev/sdb3?"
+read -p "new partition path: " -e -i /dev/sdb3 PAR
+echo "Creating an ext4 filesystem on the new partition"
+mkfs.ext4 -L persistence $PAR
+# wait
+sleep 5
+echo "Making sure the disk is labeled"
+e2label $PAR persistence
+echo "Creating a persistence.conf file"
+mkdir -p /mnt/storage
+mount $PAR /mnt/storage
+echo "/ union" > /mnt/storage/persistence.conf
+umount $PAR
+echo \n
+echo \n
+echo \n
+echo "DONE!"
