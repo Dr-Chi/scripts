76dbfadd3b9b300c31e4d625929ef18b43ff14b7
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-06-20 22:14:30 -0400

    Update newsetup.sh
    Initial work

--- linux/newsetup.sh
+++ linux/newsetup.sh
@@ -1,3 +1,15 @@
-# ~/.bash_aliases
+#!/bin/bash
+#
+# https://github.com/Dr-Chi/scripts/master/linux/newsetup.sh
+#
+# Copyright (c) 2019 DrChi. Released under the MIT License.
+#
+# One-liner to call this script from terminal:
+# wget https://github.com/Dr-Chi/scripts/master/linux/newsetup.sh -O newsetup.sh && bash newsetup.sh
+
+# Add to ~/.bash_aliases
+cat <<EOT >> ~/.bash_aliases
 alias runmobsf='sudo docker run -it -p 8000:8000 -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
 alias editmobsf='sudo docker run -it --entrypoint /bin/bash -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
+alias screenscale='xrandr --output eDP-1 --scale 1.4x1.4 --panning 3584x2016+0+0'
+EOT
