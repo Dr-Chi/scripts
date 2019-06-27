e0e634ecef303d93b1fd00b6b82b61fd0f16bc7e
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-06-26 21:17:49 -0400

    Update newsetup.sh
    trying a different insert for aliases

--- linux/newsetup.sh
+++ linux/newsetup.sh
@@ -8,8 +8,10 @@
 # wget https://raw.githubusercontent.com/Dr-Chi/scripts/master/linux/newsetup.sh -O newsetup.sh && bash newsetup.sh
 
 # Add to ~/.bash_aliases
-cat <<EOT >> ~/.bash_aliases
-alias runmobsf='sudo docker run -it -p 8000:8000 -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
-alias editmobsf='sudo docker run -it --entrypoint /bin/bash -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
-alias screenscale='xrandr --output eDP-1 --scale 1.4x1.4 --panning 3584x2016+0+0'
-EOT
+
+#cat <<EOT >> ~/.bash_aliases
+#alias runmobsf='sudo docker run -it -p 8000:8000 -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
+#alias editmobsf='sudo docker run -it --entrypoint /bin/bash -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'
+#alias screenscale='xrandr --output eDP-1 --scale 1.4x1.4 --panning 3584x2016+0+0'
+#EOT
+grep -qxF 'alias runmobsf"' ~/.bash_aliases || echo alias runmobsf='sudo docker run -it -p 8000:8000 -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest' >> ~/.bash_aliases
