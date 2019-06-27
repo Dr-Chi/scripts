61c00406cc853423830b77f28d20f95e21a018db
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2019-06-27 19:40:03 -0400

    Update newsetup.sh

--- linux/newsetup.sh
+++ linux/newsetup.sh
@@ -17,12 +17,26 @@
 		"alias runmobsf"
 		"alias editmobsf"
 		"alias mount"
+		"alias ports"
+		"alias rm"
+		"alias mv"
+		"alias cp"
+		"alias ln"
+		"alias reboot"
 	)
 	aliases=(
 		'alias screenscale='"'"'xrandr --output eDP-1 --scale 1.4x1.4 --panning 3584x2016+0+0'"'"''
 		'alias runmobsf='"'"'sudo docker run -it -p 8000:8000 -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'"'"''
 		'alias editmobsf='"'"'sudo docker run -it --entrypoint /bin/bash -v MobSFData:/root/.MobSF opensecurity/mobile-security-framework-mobsf:latest'"'"''
 		'alias mount='"'"'mount |column -t'"'"''
+		'alias ports='"'"'netstat -tulanp'"'"''
+		# do not delete / or prompt if deleting more than 3 files at a time #
+		'alias rm='"'"'rm -I --preserve-root'"'"''
+		# confirmation #
+		'alias mv='"'"'mv -i'"'"''
+		'alias cp='"'"'cp -i'"'"''
+		'alias ln='"'"'ln -i'"'"''
+		'alias reboot='"'"'shurdown -r now'"'"''
 	)
 
 	#for ((i=0;i<${#toGrep[@]};i++))  #this works but I prefer "for i in"
