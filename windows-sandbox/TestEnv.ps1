f256491c9dbd92530188d2ae0fe17dc0e7043d13
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2022-04-15 11:19:18 -0400

    Update TestEnv.ps1

--- windows-sandbox/TestEnv.ps1
+++ windows-sandbox/TestEnv.ps1
@@ -1,8 +1,10 @@
 # Install The Following Applications:
 
 # Antivirus
-winget install --id=ClamWin.ClamWin  -e --accept-package-agreements
-winget install --id=Bitdefender.Bitdefender  -e --accept-package-agreements
+winget install --id=VirusTotal.VirusTotalUploader  -e
+<# Takes forever to install #>winget install --id=ClamWin.ClamWin  -e --accept-package-agreements
+<# errors out after install #>winget install --id=Bitdefender.Bitdefender  -e --accept-package-agreements
+
 
 # MalwareBytes
 winget install --id=Malwarebytes.Malwarebytes  -e --accept-package-agreements
