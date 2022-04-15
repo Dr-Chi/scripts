4749c52a4011b3ae4e85037f267343ca5117b73a
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2022-04-15 11:00:43 -0400

    Create TestEnv.ps1

---
+++ windows-sandbox/TestEnv.ps1
@@ -0,0 +1,11 @@
+# Install The Following Applications:
+
+# Antivirus
+winget install --id=ClamWin.ClamWin  -e --accept-package-agreements
+winget install --id=Bitdefender.Bitdefender  -e --accept-package-agreements
+
+# MalwareBytes
+winget install --id=Malwarebytes.Malwarebytes  -e --accept-package-agreements
+
+# Atom
+winget install --id=GitHub.Atom  -e --accept-package-agreements
