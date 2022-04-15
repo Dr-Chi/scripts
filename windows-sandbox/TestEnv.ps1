3c6ef4a030cc35dce58d99f20b7e217ee431c724
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2022-04-15 11:49:27 -0400

    Update TestEnv.ps1

--- windows-sandbox/TestEnv.ps1
+++ windows-sandbox/TestEnv.ps1
@@ -1,13 +1,13 @@
 # Install The Following Applications:
 
 # Antivirus
-winget install --id=VirusTotal.VirusTotalUploader  -e
-<# Takes forever to install #>winget install --id=ClamWin.ClamWin  -e --accept-package-agreements
-<# errors out after install #>winget install --id=Bitdefender.Bitdefender  -e --accept-package-agreements
+winget install --id=VirusTotal.VirusTotalUploader  -e --accept-package-agreements --accept-source-agreements
+<# Takes forever to install #>winget install --id=ClamWin.ClamWin  -e --accept-package-agreements --accept-source-agreements
+<# errors out after install #>winget install --id=Bitdefender.Bitdefender  -e --accept-package-agreements --accept-source-agreements
 
 
 # MalwareBytes
-winget install --id=Malwarebytes.Malwarebytes  -e --accept-package-agreements
+winget install --id=Malwarebytes.Malwarebytes  -e --accept-package-agreements --accept-source-agreements
 
 # Atom
-winget install --id=GitHub.Atom  -e --accept-package-agreements
+winget install --id=GitHub.Atom  -e --accept-package-agreements --accept-source-agreements
