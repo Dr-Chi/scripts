190ad7264c79a69fd16c15332ba1c9c0ee650945
Dr-Chi <47183855+Dr-Chi@users.noreply.github.com>
2023-01-13 12:51:18 -0500

    simplified everything
    removed many variables that were not needed and condensed as much as possible

--- mouse_jiggler.py
+++ mouse_jiggler.py
@@ -3,90 +3,38 @@ import time
 from datetime import datetime
 from pynput import mouse, keyboard
 
+# Disable cursor's screen corner failsafe
+mouse.Listener.FAILSAFE = False
+
 # Print a message to indicate that the script is running
 print("...")
 
-# Initialize a variable to keep track of the direction the cursor is moving
-forward = True
-
-# Initialize a variable to store the time of the last input event
-last_input_time = time.time()
-
-# Define a function to check for input events
-def on_move(x, y):
-    global last_input_time
-    last_input_time = time.time()
-
-# Define a function to check for input events
-def on_click(x, y, button, pressed):
-    global last_input_time
-    last_input_time = time.time()
-
-# Define a function to check for input events
-def on_scroll(x, y, dx, dy):
-    global last_input_time
-    last_input_time = time.time()
-
-# Define a function to check for input events
-def on_press(key):
-    global last_input_time
-    last_input_time = time.time()
-
-# Define a function to check for input events
-def on_release(key):
-    global last_input_time
-    last_input_time = time.time()
-
 # Start listening to input events
-with mouse.Listener(on_move=on_move,on_click=on_click,on_scroll=on_scroll) as mouse_listener:
-    with keyboard.Listener(on_press=on_press,on_release=on_release) as keyboard_listener:
+#store the last input event time in the mouse listener
+with mouse.Listener(on_move=lambda x, y: time.time(),on_click=lambda x, y, button, pressed: time.time(),on_scroll=lambda x, y, dx, dy: time.time()) as mouse_listener:
+    #store the last input event time in the keyboard listener
+    with keyboard.Listener(on_press=lambda key: time.time(),on_release=lambda key: time.time()) as keyboard_listener:
         try:
+            #initialize the x and y position of the cursor
+            x_prev, y_prev = mouse.Controller().position
             while True:
-                # Get the current position of the cursor
-                xpos, ypos = mouse.Controller().position
-
-                # Calculate the new position for the cursor, based on the direction it's moving
-                new_pos = xpos - 1
-                if forward:
-                    new_pos = xpos + 1
-
-                # Change the direction of the cursor's movement
-                forward = not forward
-
-                # Move the cursor to the new position
-                mouse.Controller().position = (new_pos, ypos)
-
-                # Generate a random sleep time between 30 and 240 seconds
-                #sleep_time = 2 # For testing
-                sleep_time = random.randint(30,240)
-
-                # Wait for the random sleep time
-                time.sleep(sleep_time)
-
+                #get the current position of the cursor
+                x, y = mouse.Controller().position
+                #determine the direction of the cursor
+                x_prev, y_prev = x, y
+                #move the cursor
+                mouse.Controller().position = (x+1 if x < x_prev else x-1, y)
+                #generate a random sleep time between 30 and 240 seconds
+                time.sleep(random.randint(30,240))
                 # check if 30 seconds has passed since the last input event
-                if time.time() - last_input_time > 30:
+                if time.time() - mouse_listener.last_input_time > 30:
                     # left-click the mouse
                     mouse.Controller().click(mouse.Button.left)
-
-                # Check the current time
+                #get the current time
                 now = datetime.now()
-
-                # Print out the mouse position; for diagnostics
-                #print(pyautogui.position())
-
-                # check if the current hour is between 8 and 15
-                if not (8 <= now.hour < 15):
-                    # generate random minute between 0 and 59
-                    random_min = random.randint(0,59)
-                    # generate random hour between 15 and 17
-                    random_hour = random.randint(15,17)
-                    # check if current hour and minute is greater than or equal to the random hour and minute
-                    if now.hour >= random_hour and now.minute >= random_min:
-                        current_time = datetime.now().strftime("%H:%M:%S")
-                        print("End ",current_time," (",random_hour,":",random_min,")")
-                        break
-
-        # If the user interrupts the script (e.g. by pressing ctrl-c), print a message and exit
+                # check if the current hour is between 8 and 15 or if the current hour and minute is greater than or equal to the random hour and minute
+                if not (8 <= now.hour < 15) or (now.hour, now.minute) >= (random.randint(15, 17), random.randint(0, 59)):
+                    print("End ",datetime.now().strftime("%H:%M:%S"))
+                    break
         except KeyboardInterrupt:
-            current_time = datetime.now().strftime("%H:%M:%S")
-            print("End ",current_time)
+            print("End ",datetime.now().strftime("%H:%M:%S"))
